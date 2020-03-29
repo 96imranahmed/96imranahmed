cefdf640541ffbdd1c9602745d58d7831892da99
Imran Ahmed <Imran_Ahmed@mckinsey.com>
2020-03-29 02:41:06 +0100

    Cleaner website

---
+++ static/js/index.js
@@ -0,0 +1,17 @@
+var options = {
+    strings: [
+        "a", 
+        "a full-time nerd",
+        "a business consultant",
+        "a machine learning fanatic",
+        "an aspiring product manager"],
+    typeSpeed: 100,
+    loop: true,
+    loopCount: Infinity,
+    showCursor: false,
+    smartBackspace: true,
+    backDelay: 1500,
+    backSpeed: 75
+};
+  
+  var typed = new Typed('#typed-intro', options);
