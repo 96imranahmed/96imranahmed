79c3782ab70a4b7a91ec40b097b1caf3044aa9a9
Imran Ahmed <Imran_Ahmed@mckinsey.com>
2020-03-31 01:15:39 +0100

    Update to website formatting logic

--- static/js/projects.js
+++ static/js/projects.js
@@ -1,6 +1,38 @@
-$(document).ready(function() {
+var $grid = null;
+let filters = new Set();
 
-    $('.tile-filter').isotope({
+function updateFilterCount() {
+    if ($grid.data('isotope').filteredItems.length == 0) {
+        $('.items-count').html(
+            '<strong>No posts</strong>')
+    } else if ($grid.data('isotope').filteredItems.length == 1) {
+        $('.items-count').html(
+            '<strong>1</strong> post')
+    } else {
+        $('.items-count').html(
+            '<strong>' + $grid.data('isotope').filteredItems.length + '</strong> posts')
+    }
+}
+
+function updateSelectedBar() {
+    $('.filter-button-group').children('button').each(function () {
+        var filterValue = $(this).attr('data-filter');
+        var parseClassName = null;
+        if (filterValue == "*") {
+            parseClassName = "filter-All"
+        } else {
+            parseClassName = "filter-" + filterValue.substring(1)
+        }
+        if (filters.has(filterValue)) {
+            $(this).addClass(parseClassName)
+        } else {
+            $(this).removeClass(parseClassName)
+        }
+    })
+}
+
+$(document).ready(function() {
+    $grid = $('.tile-filter').isotope({
         // options
         itemSelector: '.tile-card',
         layoutMode: 'masonry',
@@ -8,10 +40,18 @@ $(document).ready(function() {
             fitWidth: true
         }
     });
-    
+    updateFilterCount();
 });
 
-$('#search-button-click').click(function() {
-    $('.tile-filter').isotope({ filter: '.abc' })
-    alert('Search button has been clicked')
-})
+$('.filter-button-group').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    if (filters.has("*") || filterValue == "*") {
+        filters.clear()
+        filters.add(filterValue)
+    } else {
+        filters.add(filterValue)
+    }
+    $grid.isotope({ filter: [...filters].join(', ') });
+    updateFilterCount()
+    updateSelectedBar()
+});
