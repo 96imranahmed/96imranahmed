eb35890f099b5aad042ff65519ba7155224a9d79
Imran Ahmed <Imran_Ahmed@mckinsey.com>
2020-04-01 11:51:40 +0100

    ScrollPrompt fixes

--- static/js/index.js
+++ static/js/index.js
@@ -15,5 +15,16 @@ var options = {
     backDelay: 1500,
     backSpeed: 60
 };
+
+function checkPosition() {
+    let windowY = window.scrollY;
+    if (windowY > ($(".scrollPrompt").position().top + 50)) {
+        if ($(".scrollPrompt").css("visibility") === "visible") {
+            $(".scrollPrompt").css("visibility", "hidden");
+        }
+    }
+}
   
-  var typed = new Typed('#typed-intro', options);
+var typed = new Typed('#typed-intro', options);
+let scrollPos = 0;
+window.addEventListener('scroll', checkPosition);
